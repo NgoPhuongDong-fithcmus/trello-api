28cf8f97687141ea819ba0d3a90e002376b3fadb
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-05-20 12:59:44 +0700

    aggregate between collections

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,13 +3,16 @@ import Joi from 'joi'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { ObjectId } from 'mongodb'
+import { BOARD_TYPES } from '~/utils/constants'
+import { columnModel } from './columnModel'
+import { cardModel } from './cardModel'
 // Define Collection (name & schema)
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
-
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
   columnOrderIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
   ).default([]),
@@ -49,11 +52,28 @@ const findOneById = async (id) => {
 
 const getDetailBoard = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(String(id))
-    })
+    // const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(String(id)) })
+    // aggregate lấy dữ liệu giống như query SQL vậy
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).aggregate([
+      { $match: {
+        _id: new ObjectId(String(id)),
+        _destroy: false
+      } },
+      { $lookup: {
+        from: columnModel.COLUMN_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'columns'
+      } },
+      { $lookup: {
+        from: cardModel.CARD_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'cards'
+      } }
+    ]).toArray()
 
-    return result
+    return result[0] || {}
   } catch (error) {
     throw new Error(error)
   }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -3,6 +3,7 @@ import { slugify } from '~/utils/formatter'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 const createNew = async (data) => {
   try {
     const newBoard = {
@@ -27,7 +28,21 @@ const getDetailBoard = async (boardId) => {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
     }
 
-    return board
+    // ------------------------BE XỬ LÍ DỮ LIỆU-----------------------------
+    // deep clone board để tạo ra board mới để xử lí, không ảnh hưởng tới board ban đầu tùy mục đích sử dụng
+    const respondBoard = cloneDeep(board)
+
+    // đưa card về đúng column của nó => biến đổi sao cho dữ liệu chuẩn chỉ bên BE xử lí hoặc FE cũng có thể xử lí
+    respondBoard.columns.forEach(column => {
+      // column.cards = respondBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+      // có thể dùng cách này
+      column.cards = respondBoard.cards.filter(card => card.columnId.equals(column._id))
+    })
+
+    delete respondBoard.cards
+    // ------------------------BE XỬ LÍ DỮ LIỆU-----------------------------
+
+    return respondBoard
   } catch (error) {
     throw error
   }
